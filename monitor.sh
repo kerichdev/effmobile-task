3e1bce07e262ed7b849806d82142ef7486998061
kerichdev <kerichuudev@gmail.com>
2024-12-10 15:06:37 +0300

    feat: implement logging

--- monitor.sh
+++ monitor.sh
@@ -1,8 +1,18 @@
 #/bin/bash
 
+# логирование
+function LOG() {
+    if [ "$1" == "info" ]; then
+        prefix="[INFO]"
+    elif [ "$1" == "error" ]; then
+        prefix="[ERROR]"
+    fi
+
+    echo -e "[$(date +'%Y-%m-%d %H:%M:%S')] $prefix $2" >> /var/log/monitor.log # не уверен, будет ли logrotate чистить наш лог - на заметку
+}
+
 if pgrep -x "test" > /dev/null; then
-    echo "test is running"
+    LOG info "test is running"
 else
-    echo "test is not running"
+    LOG error "test is not running"
 fi
-sleep 1
